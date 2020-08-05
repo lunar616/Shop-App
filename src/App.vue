a8bc0a50b4ef901c2b5f75c871c56fbcb70d1e4a
lunar616 <lunar616vue@yandex.ru>
2020-08-05 17:15:47 +0300

    fixed: изменил title

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@
       <div class="navbar">
         <div class="container">
           <div class="navbar-content">
-            <div class="logo">VUE-CLI</div>
+            <div class="logo">SHOP-APP</div>
             <ul class="navbar-list">
               <li class="navbar-item" v-for="link in links" :key="link.title">
                 <router-link class="navbar-link" :title="link.title" :to="link.url">{{ link.title }}</router-link>
