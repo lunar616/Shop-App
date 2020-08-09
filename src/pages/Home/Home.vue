99274ce81fdc3cdc0b11a1046dae510fbfd12b38
lunar616 <lunar616vue@yandex.ru>
2020-08-09 16:06:47 +0300

    fixed: исправил ошибку в тексте

--- src/pages/Home/Home.vue
+++ src/pages/Home/Home.vue
@@ -8,7 +8,7 @@
           <h2 class="home-title">В этом приложении:</h2>
           <p class="home-text">- Работа с Vue-Router</p>
           <p class="home-text">- Создание динамических маршрутов</p>
-          <p class="home-text">- Хранение и взаимодействие с данных через Vuex</p>
+          <p class="home-text">- Хранение и взаимодействие с данными через Vuex</p>
           <p class="home-text">- Использование библиотеки Vue Carousel</p>
         </div>
       </div>
